2379fe63fc86f828f0e61ffe332ab92070e11ad0
umutuzgur <umut.uzgur@ozu.edu.tr>
2014-04-10 11:17:30 +0300

    Tweet class added

---
+++ code/src/DataRequester/Tweet.java
@@ -0,0 +1,44 @@
+package DataRequester;
+
+
+public class Tweet {
+	private String content;
+	private String date;
+	private String userName;
+	private long userId;
+	private long tweetId;
+	public Tweet(){
+		
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public long getTweetId() {
+		return tweetId;
+	}
+	public void setTweetId(long tweetId) {
+		this.tweetId = tweetId;
+	}
+	
+}
