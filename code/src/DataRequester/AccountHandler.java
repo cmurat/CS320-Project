d5adc229b83a99c5fbfb22adfa1b3ef35a2cf8d7
Çelebi Murat <celebi.murat@ozu.edu.tr>
2014-04-13 02:11:44 +0300

    -Minor import changes

--- code/src/DataRequester/AccountHandler.java
+++ code/src/DataRequester/AccountHandler.java
@@ -8,7 +8,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
