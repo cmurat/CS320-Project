c7aa4cf02a77bc69132b4fccd40033a479cfbbce
Çelebi Murat <celebi.murat@ozu.edu.tr>
2014-04-12 03:26:59 +0300

    -Working on TtweetBox of GUI Part

--- code/src/GUI/TweetBox.java
+++ code/src/GUI/TweetBox.java
@@ -1,5 +1,35 @@
 package GUI;
 
-public class TweetBox {
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.TextField;
 
+import javax.swing.JPanel;
+
+public class TweetBox extends JPanel{
+	
+	private MainPanel mainPanel;
+	private TextField tweetField;
+	
+	public TweetBox(MainPanel mainPanel) {
+		this.mainPanel = mainPanel;
+		init();
+	}
+	
+	private void init() {
+		setMaximumSize(new Dimension(mainPanel.getWidth(), mainPanel.getHeight()/20));
+		setBounds(mainPanel.getBounds());
+		setLayout(new FlowLayout());
+		addTweetField();
+	}
+	
+	private void addTweetField() {
+		tweetField = new TextField();
+		tweetField.setBounds(getBounds());
+		tweetField.setText("Sev Beni");
+	}
+	
+	public String getTweetText(){
+	}
+	
 }
---
+++ code/src/GUIManager/listeners/TweetBoxListener.java
@@ -0,0 +1,18 @@
+package GUIManager.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import GUI.TweetBox;
+
+public class TweetBoxListener implements ActionListener{
+	private TweetBox tweetBox;
+	public TweetBoxListener(TweetBox tweetBox){
+		this.tweetBox = tweetBox;
+	}
+	
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+
+}
